b6481008e0cbc9d380cb8a038ae824913c080114
Hilal Mufti <hmufti@cs.washington.edu>
2025-11-09 11:03:42 -0800

    Working s-expression printer

--- engine.c
+++ engine.c
@@ -9,7 +9,7 @@ typedef struct Value Value;
 typedef enum {
     VAL_ADD,
     VAL_MUL,
-    VAL_CONST
+    VAL_FLOAT
 } ValueType;
 
 /* Can I just make this a normal Expr? */
@@ -20,19 +20,19 @@ struct Value {
     Value *prev2;
 };
 
-char STRINGS[STRCAP];
+char STRTAB[STRCAP];
 unsigned int allocated;
 
-void strsdbg(unsigned int beg, unsigned int end) {
+void stdbg(unsigned int beg, unsigned int end) {
     for (int i = beg; i < end; i++)
-        printf("%d [0x%ld]: %c\n", i, STRINGS + i, STRINGS[i]);
+        printf("%d [0x%ld]: %c\n", i, STRTAB + i, STRTAB[i]);
 }
 
 void valcheck(Value *v) {
     assert(v != NULL);
     assert(v->op == VAL_ADD ||
            v->op == VAL_MUL ||
-           v->op == VAL_CONST);
+           v->op == VAL_FLOAT);
     assert((v->prev1 != NULL && v->prev2 != NULL) ||
            (v->prev1 != NULL && v->prev2 == NULL) ||
            (v->prev1 == NULL && v->prev2 == NULL));
@@ -60,29 +60,29 @@ char *vtshow(ValueType vt) {
 
     switch (vt) {
         case VAL_ADD:
-            n = snprintf(STRINGS + allocated, STRCAP, "%s", "add");
+            n = snprintf(STRTAB + allocated, STRCAP, "%s", "add"); /* TODO: refactor global strtable printing */
 
             assert(n >= 0);
-            assert(allocated + n + 1 < STRCAP); /* TODO: make this tight */
+            assert(allocated + n + 1 < STRCAP);
 
             allocated += n + 1;
-            return STRINGS + old_allocated;
+            return STRTAB + old_allocated;
         case VAL_MUL:
-            n = snprintf(STRINGS + allocated, STRCAP, "%s", "mul");
+            n = snprintf(STRTAB + allocated, STRCAP, "%s", "mul");
 
             assert(n >= 0);
             assert(allocated + n + 1 < STRCAP);
 
             allocated += n + 1;
-            return STRINGS + old_allocated;
-        case VAL_CONST:
-            n = snprintf(STRINGS + allocated, STRCAP, "%s", "constant");
+            return STRTAB + old_allocated;
+        case VAL_FLOAT:
+            n = snprintf(STRTAB + allocated, STRCAP, "%s", "float");
 
             assert(n >= 0);
             assert(allocated + n + 1 < STRCAP);
 
             allocated += n + 1;
-            return STRINGS + old_allocated;
+            return STRTAB + old_allocated;
         default:
             assert(false);
     }
@@ -102,7 +102,7 @@ char *valshow(Value *v) {
         char *s2 = valshow(v->prev2);
         old_allocated = allocated;
 
-        n = snprintf(STRINGS + allocated, STRCAP,
+        n = snprintf(STRTAB + allocated, STRCAP,
                  "Value(op=%s, data=%.2f, prev1=%s, prev2=%s)",
                  op,
                  v->data,
@@ -113,13 +113,13 @@ char *valshow(Value *v) {
         assert(allocated + n + 1 < STRCAP);
 
         allocated += n + 1;
-        return STRINGS + old_allocated;
+        return STRTAB + old_allocated;
     } else if (v->prev1 != NULL && v->prev2 == NULL) {
         char *op = vtshow(v->op);
         char *s = valshow(v->prev1);
         old_allocated = allocated;
 
-        n = snprintf(STRINGS + allocated, STRCAP,
+        n = snprintf(STRTAB + allocated, STRCAP,
                  "Value(op=%s, data=%.2f, prev1=%s, prev2=NULL)",
                  op,
                  v->data,
@@ -129,12 +129,12 @@ char *valshow(Value *v) {
         assert(allocated + n + 1 < STRCAP);
 
         allocated += n + 1;
-        return STRINGS + old_allocated;
+        return STRTAB + old_allocated;
     } else if (v->prev1 == NULL && v->prev2 == NULL) {
         char *op = vtshow(v->op);
         old_allocated = allocated;
 
-        n = snprintf(STRINGS + allocated, STRCAP,
+        n = snprintf(STRTAB + allocated, STRCAP,
                  "Value(op=%s, data=%.2f, prev1=NULL, prev2=NULL)",
                  op,
                  v->data);
@@ -143,7 +143,7 @@ char *valshow(Value *v) {
         assert(allocated + n + 1 < STRCAP);
 
         allocated += n + 1;
-        return STRINGS + old_allocated;
+        return STRTAB + old_allocated;
     } else
         assert(false);
 }
@@ -152,16 +152,58 @@ char *valsexpr(Value *v) {
     int n;
     unsigned int old_allocated;
 
-    char *s = vtshow(v->op);
-    old_allocated = allocated;
-    n = snprintf(STRINGS + allocated, STRCAP, "(%s %.2f)", s, v->data);
+    valcheck(v);
 
-    assert(n >= 0);
-    assert(allocated + n + 1 < STRCAP);
+    if (v->prev1 != NULL && v->prev2 != NULL) {
+        char *op = vtshow(v->op);
+        char *s1 = valsexpr(v->prev1);
+        char *s2 = valsexpr(v->prev2);
+        old_allocated = allocated;
 
-    allocated += n + 1;
+        n = snprintf(STRTAB + allocated, STRCAP,
+                 "(%s %s %s :%.2f)",
+                 op,
+                 s1,
+                 s2,
+                 v->data);
 
-    return STRINGS + old_allocated;
+        assert(n >= 0);
+        assert(allocated + n + 1 < STRCAP);
+
+        allocated += n + 1;
+        return STRTAB + old_allocated;
+    } else if (v->prev1 != NULL && v->prev2 == NULL) {
+        char *op = vtshow(v->op);
+        char *s = valsexpr(v->prev1);
+        old_allocated = allocated;
+
+        n = snprintf(STRTAB + allocated, STRCAP,
+                 "(%s %s :%.2f)",
+                 op,
+                 s,
+                 v->data);
+
+        assert(n >= 0);
+        assert(allocated + n + 1 < STRCAP);
+
+        allocated += n + 1;
+        return STRTAB + old_allocated;
+    } else if (v->prev1 == NULL && v->prev2 == NULL) {
+        char *op = vtshow(v->op);
+        old_allocated = allocated;
+
+        n = snprintf(STRTAB + allocated, STRCAP,
+                 "(%s %.2f)",
+                 op,
+                 v->data);
+
+        assert(n >= 0);
+        assert(allocated + n + 1 < STRCAP);
+
+        allocated += n + 1;
+        return STRTAB + old_allocated;
+    } else
+        assert(false);
 }
 
 Value *valadd(Value *x, Value *y, Value *ret) {
@@ -187,7 +229,7 @@ float f(float x) {
 }
 
 int main(int argc, char **argv) {
-    memset(STRINGS, 0, STRCAP);
+    memset(STRTAB, 0, STRCAP);
     allocated = 0;
 
     printf("%.3f\n", f(3.0));
@@ -198,25 +240,24 @@ int main(int argc, char **argv) {
     Value ret = {0};
     Value ret2 = {0};
 
-    valinit(&a, VAL_CONST, 2.0, NULL, NULL);
-    valinit(&b, VAL_CONST, -3.0, NULL, NULL);
-    valinit(&c, VAL_CONST, 10.0, NULL, NULL);
+    valinit(&a, VAL_FLOAT, 2.0, NULL, NULL);
+    valinit(&b, VAL_FLOAT, -3.0, NULL, NULL);
+    valinit(&c, VAL_FLOAT, 10.0, NULL, NULL);
 
-    printf("a = %s\n", valshow(&a));
+    printf("a = %s\n", valsexpr(&a));
 
-    printf("b = %s\n", valshow(&b));
+    printf("b = %s\n", valsexpr(&b));
 
-    printf("c = %s\n", valshow(&c));
+    printf("c = %s\n", valsexpr(&c));
 
-    printf("a + b = %s\n", valshow(valadd(&a, &b, &ret)));
+    printf("a + b = %s\n", valsexpr(valadd(&a, &b, &ret)));
 
-    printf("a * b = %s\n", valshow(valmul(&a, &b, &ret)));
+    printf("a * b = %s\n", valsexpr(valmul(&a, &b, &ret)));
 
-    /* Value(op=add, data=-1.00, prev1=Value(op=mul, data=-6.00, prev1=Value(op=constant, data=2.00, prev1=NULL, prev2=NULL), prev2=Value(op=constant, data=-3.00, prev1=NULL, prev2=NULL)), prev2=Value(op=constant, data=10.00, prev1=NULL, prev2=NULL)) */
     /* valadd(valmul(&a, &b, &ret), &c, &ret); */
-    printf("a * b + c = %s\n", valshow(valadd(valmul(&a, &b, &ret), &c, &ret2)));
+    printf("a * b + c = %s\n", valsexpr(valadd(valmul(&a, &b, &ret), &c, &ret2)));
 
-    strsdbg(0, 1024);
+    /* dbg(0, 1024); */
 
 }
 
@@ -228,6 +269,7 @@ int main(int argc, char **argv) {
  * - [ ] Support tensors
  * - [ ] Add Python bindings
  * - [ ] Add Racket/Scheme bindings
+ * - [ ] Setup tests
  * - [ ] Systems-level performance optimization
  *   - [ ] DoD
  *   - [ ] Pooled arena allocation
